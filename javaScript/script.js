fb5396c736c0d0b9b1ef12a1a738aae5ff132908
breno-aredes <breno_mg10@hotmail.com>
2022-10-28 12:53:32 -0300

    v 0.2.2 (horario local e nome nas mensagens)

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,17 +1,27 @@
 
 let listaMensagens = [];
+let horario = '';
+const nome = prompt('Seu Lindo nome');
+
+//funçaõ para salvar o horario no computador do usuario
+function hora() {
+    horario = new Date().toLocaleTimeString();
+}
 
 //funcão para enviar a mensagem
 function enviarMensagem() {
     //salva o valor digitado
-    let mensagem = document.querySelector('.mensagem').value;
+    const mensagem = document.querySelector('.mensagem').value;
     //salva todas mensagens presentes no site
-    listaMensagens = document.querySelector("ul")
+    listaMensagens = document.querySelector("ul");
+    // chama a função para verificar a hora.
+    hora();
     //adiciona a mensagem nova
     listaMensagens.innerHTML = listaMensagens.innerHTML +
         `<li class="global">
-        <h1><time>(09:22:28)</time> <strong>João</strong> para <strong>Todos</strong>: ${mensagem} </h1>
+        <h1><time>(${horario})</time> <strong>${nome}</strong> para <strong>Todos</strong>: ${mensagem} </h1>
     </li>`;
     //remove a mensagem do input para uma nova ser digitada
     document.querySelector('.mensagem').value = '';
+
 }
